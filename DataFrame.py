7f4694f44ae84e382d485d14f44fc8ce31aa132d
ruslan.velikij <velikyruslan@gmail.com>
2025-07-14 15:52:26 +0300

    add folder datasets, shape DataFrame

--- DataFrame.py
+++ DataFrame.py
@@ -1,4 +1,15 @@
 import pandas as pd
 import numpy as np
-print(pd.__version__)
-print(np.__version__)
+
+col_names = ["id", "cycle", "setting1", "setting2", "setting3"] + [f"sensor{i}" for i in range(1, 22)]
+train_df = pd.read_csv("datasets/train_FD001.txt", sep=r'\s+', header=None, names=col_names)
+print("Размер обучающего набора:", train_df.shape)
+print("Первые 5 строк:")
+print(train_df.head())
+
+max_cycle = train_df.groupby("id")["cycle"].max()
+train_df = train_df.merge(max_cycle.to_frame(name="max_cycle"), on="id")
+train_df["RUL"] = train_df["max_cycle"] - train_df["cycle"]
+train_df.drop("max_cycle", axis=1, inplace=True)
+print("\nПосле добавления RUL:")
+print(train_df[["id", "cycle", "RUL"]].head(10))
